bf87d626470da45a9fa744be2736c6ce463afcaf
Antonio Possemato <antoniopossemato@gmail.com>
2026-05-08 18:15:40 +0200

    Aggiornato modello User con specifiche UML

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    //ATTRIBUTI CLASSE UTENTE
+    // ATTRIBUTI CLASSE UTENTE (Superclasse)
     email: { 
         type: String, 
         required: true, 
@@ -14,41 +15,60 @@ const userSchema = new mongoose.Schema({
         required: true, 
         minlength: 8 
     },
-    nome: { type: String, required: true },
-    cognome: { type: String, required: true },
-    dataNascita: { type: Date },
-    codiceFiscale: { type: String, uppercase: true },
-    
-    //Gestione Ruoli (Discriminatore)
     ruolo: { 
         type: String, 
         enum: ['cittadino', 'proprietario', 'operatore'], 
         default: 'cittadino' 
     },
+    autenticato: { 
+        type: Boolean, 
+        default: false 
+    },
+    notifiche: [{
+        messaggio: String,
+        letta: { type: Boolean, default: false },
+        data: { type: Date, default: Date.now }
+    }],
 
-    //ATTRIBUTI CLASSE CITTADINO
+    // ATTRIBUTI CLASSE CITTADINO (Sottoclasse)
+    nome: { 
+        type: String, 
+        required: function() { return this.ruolo === 'cittadino'; } 
+    },
+    cognome: { 
+        type: String, 
+        required: function() { return this.ruolo === 'cittadino'; } 
+    },
+    scoreAffidabilita: { 
+        type: Number, 
+        default: 0.0 
+    },
+    posizione: {
+        latitudine: { type: Number },
+        longitudine: { type: Number }
+    },
     profiloDisabilita: { 
         type: String, 
         enum: ['motoria', 'visiva', 'uditiva', 'nessuna'], 
         default: 'nessuna' 
     },
-    telefono: { type: String },
-    
-    //liste di riferimento (ID che puntano ad altre collezioni)
-    segnalazioniInviate: [{ 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Segnalazione' 
-    }],
-    itinerariSalvati: [{ 
+    storicoPercorsi: [{ 
         type: mongoose.Schema.Types.ObjectId, 
         ref: 'Percorso' 
     }],
-    notifiche: [{
-        messaggio: String,
-        letta: { type: Boolean, default: false },
-        data: { type: Date, default: Date.now }
+    storicoSegnalazioni: [{ 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Segnalazione' 
     }]
 
 }, { timestamps: true });
+
+
+// Middleware: Hashing della password prima del salvataggio
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
 
 module.exports = mongoose.model('User', userSchema);
