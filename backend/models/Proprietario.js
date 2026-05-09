707c31020c22410899c45a8e3053c31c8f4af071
Andrea Boarini <andreaboa26@gmail.com>
2026-05-09 10:43:02 +0200

    feat: aggiunta sottoclasse ProprietarioStruttura estensione di User

---
+++ backend/models/Proprietario.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const User = require('./User');
+
+const proprietarioStrutturaSchema = new mongoose.Schema({
+    // attributi classe proprietario (estende User)
+    partitaIVA: {
+        type: String,
+        required: [true, 'campo partita IVA obbligatorio'],
+        unique: true,
+        trim: true,
+        match: [/^\d{11}$/, 'formato partita IVA non valido'] // la partita IVA deve essere di 11 cifre
+    },
+    ragioneSociale: {
+        type: String,
+        required: [true, 'campo ragione sociale obbligatorio'],
+    },
+    strutture: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'StrutturaPrivata'
+    }]
+});
+
+module.exports = User.discriminator('Proprietario', proprietarioStrutturaSchema);
