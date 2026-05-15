ea6b94aecea713f25bb7388ed35ba256d5095612
antpss <antoniopossemato@gmail.com>
2026-05-15 15:31:26 +0200

    fix: funzione validazione in SegnalazionePubblica.js

--- backend/models/SegnalazionePubblica.js
+++ backend/models/SegnalazionePubblica.js
@@ -1,22 +1,19 @@
-
 const mongoose = require('mongoose');
 const Segnalazione = require('./Segnalazione');
 
 const segnalazionePubblicaSchema = new mongoose.Schema({
-    //attributi specifici
+    // attributi specifici
     enteCompetente: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',     //DA CAMBIARE IN OPERATORECOMUNE QUANDO SI MODELLERÀ
-        default: null    //null finché non viene assegnato un operatore
+        ref: 'User',     // DA CAMBIARE IN OPERATORECOMUNE QUANDO SI MODELLERÀ
+        default: null    // null finché non viene assegnato un operatore
     }
 });
 
-
-segnalazionePubblicaSchema.pre('validate', function(next) {
+segnalazionePubblicaSchema.pre('validate', function() {
     if (this.stato === 'IN_VERIFICA') {
-        return next(new Error('una SegnalazionePubblica non può essere in stato IN_VERIFICA'));
+        throw new Error('Una Segnalazione Pubblica non può mai trovarsi in stato IN_VERIFICA');
     }
-    next();
 });
 
 module.exports = Segnalazione.discriminator('pubblica', segnalazionePubblicaSchema);
